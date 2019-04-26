85ab0f84ff3653f4c50bff8f9d295d29fe1d429a
张欣如 <xzhan63@stevens.edu>
2019-04-26 16:54:28 -0400

    551 project

---
+++ 551_project/picture/plane_main.py
@@ -0,0 +1,245 @@
+import random
+import  pygame
+from plane_sprites import *
+score = 0
+ENEMY_SCORE = 100
+BOSSBORN = False
+game_font_start = pygame.font.SysFont('arial',30,True)
+game_font_over = pygame.font.SysFont('arial',50,True)
+
+
+class AvengersWar(object):
+    """main_avengers_war"""
+
+    def __init__(self):
+        # 1. Build a window_screen
+        self.screen = pygame.display.set_mode(SCREEN_RECT.size)
+        # 2. Set time_clock
+        self.clock = pygame.time.Clock()
+        # 3.Call private methods and create sprites_Groups
+        self.__create__sprites()
+
+        # 4.Set timer events
+        pygame.time.set_timer(CREATE_ENEMY_EVENT,1500)
+        pygame.time.set_timer(HERO_FIRE_EVENT,600)
+        pygame.time.set_timer(CREATE_BOSS_EVENT,5000)
+        pygame.time.set_timer(CREATE_BUBBLE_EVENT,5000)
+
+    def create_background(self):
+        # Create background sprite ans corresponding sprite_Group
+        bg1 = Background()
+        bg2 = Background(True)
+        self.back_group = pygame.sprite.Group(bg1, bg2)
+        self.back_group.update()
+
+    def __create__sprites(self):
+
+        self.create_background()
+        self.back_group.draw(self.screen)
+        pygame.display.update()
+
+        while True:
+            # There are two superheroes that the players can select.Press 1: Spiderman. Press 2: Ironman
+            keys_pressed = pygame.key.get_pressed()
+            pygame.key.get_mods()
+            self.screen.blit(game_font_over.render(u'AvengersWar' , True, [255, 0, 0]), [450, 100])
+            self.screen.blit(game_font_start.render(u'Choose your favorite superhero' , True, [0, 255, 0]), [400, 250])
+            bg = pygame.image.load("./picture/spiderman11.png")
+            self.screen.blit(game_font_start.render(u'Press 1' , True, [0, 255, 0]), [300, 400])
+            bg1 = pygame.image.load("./picture/ironmanm.png")
+            self.screen.blit(game_font_start.render(u'Press 2' , True, [0, 255, 0]), [900, 400])
+
+            self.screen.blit(bg, (150, 300))
+            self.screen.blit(bg1, (750, 300))
+            pygame.display.update()
+            if keys_pressed[pygame.K_1]:
+                path = "./picture/spiderman11.png"
+                break
+            if keys_pressed[pygame.K_2]:
+                path = "./picture/ironmanm.png"
+                break
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    AvengersWar.__game_over()
+
+        # Create an enemies--UFOs sprite Group
+        self.enemy_group = pygame.sprite.Group()
+
+        # Create a superheroes sprite Group
+        self.hero = Hero(path)
+        self.hero_group = pygame.sprite.Group(self.hero)
+
+        # Create an enemy_bullets_hit sprite Group
+        self.enemyb_hit_group = pygame.sprite.Group()
+
+        # Create a boss sprite Group
+        self.boss = Boss()
+        self.boss_group = pygame.sprite.Group()
+
+    def start_game(self):
+        print('game starts')
+
+
+        while True:
+
+            self.clock.tick(FRAME_PER_SEC)
+            self.event__handler()
+            self.__check_collider()
+            self.__update_sprites()
+            pygame.display.update()
+
+    def event__handler(self):
+        global BOSSBORN
+        for event in pygame.event.get():
+            # Judge quit or not
+            if event.type == pygame.QUIT:
+                AvengersWar.__game_over()
+
+            if event.type == CREATE_ENEMY_EVENT and len(self.boss_group)==0:
+                # Instantiate enemy
+                enemy = Enemy()
+                # Add Enemy sprite to Enemy_Group
+                self.enemy_group.add(enemy)
+
+            if event.type == HERO_FIRE_EVENT:
+                self.hero.fire()
+
+            if event.type == CREATE_BOSS_EVENT and BOSSBORN == False:
+                BOSSBORN = True
+                self.boss_group.add(self.boss)
+                self.boss.exist=True
+
+            if self.boss.exist and event.type == CREATE_BUBBLE_EVENT:
+                self.boss.bubble()
+
+        # Get key_pressed
+        keys_pressed = pygame.key.get_pressed()
+        pygame.key.get_mods()
+        # Judge the corresponding index
+        self.move(keys_pressed)
+
+    def move(self,keys_pressed):
+        if keys_pressed[pygame.K_RIGHT] or keys_pressed[pygame.K_LEFT] or keys_pressed[pygame.K_DOWN] or keys_pressed[pygame.K_UP]:
+            self.move_right(keys_pressed[pygame.K_RIGHT])
+            self.move_left(keys_pressed[pygame.K_LEFT])
+            self.move_up(keys_pressed[pygame.K_UP])
+            self.move_down(keys_pressed[pygame.K_DOWN])
+        elif pygame.KEYUP:
+            self.hero.speed = 0
+            self.hero.speedy = 0
+
+    def move_right(self,keys_pressed):
+        if keys_pressed:
+            self.hero.speed = 10
+
+    def move_left(self,keys_pressed):
+        if keys_pressed:
+            self.hero.speed = -10
+
+    def move_down(self, keys_pressed):
+        if keys_pressed:
+            self.hero.speedy = 10
+
+    def move_up(self, keys_pressed):
+        if keys_pressed:
+            self.hero.speedy = -10
+
+    def __check_collider(self):
+
+        # 1.enemy_bullet_hit
+        # Show the explosion.
+        enemy_bullet_hit_dict = pygame.sprite.groupcollide(self.hero.bullets,self.enemy_group,True,False)
+        global score
+        global ENEMY_SCORE
+        score += len(enemy_bullet_hit_dict)*ENEMY_SCORE
+        self.enemyb_hit_group.add(enemy_bullet_hit_dict.values())
+        # print(enemy_bullet_hit_dict.values())
+        for enemy1 in self.enemyb_hit_group:
+            if enemy1.explode_index ==0:
+                # The index that call the explosion pictures method
+                enemy1.explode_index = 1
+
+                # The index that delete the spirte
+            elif enemy1.explode_index == 4:
+                self.enemyb_hit_group.remove_internal(enemy1)
+                self.enemy_group.remove_internal(enemy1)
+
+        # 2. ememies_hero_hit
+        enemies = pygame.sprite.spritecollide(self.hero,self.enemy_group,True)
+        # 3.bubbles_hero_hit
+        bubbles = pygame.sprite.spritecollide(self.hero, self.boss.weapons, True)
+        #If exists collider_event(enemies or bubles), delete the superhero.
+
+        if len(enemies) > 0 or len(bubbles)>0:
+            for hero1 in self.hero_group:
+                if self.hero.mainexplode_index == 0:
+                    self.hero.mainexplode_index =1
+        if self.hero.mainexplode_index == 4:
+            self.hero.kill()
+            self.__game_over()
+
+        # 4. bullets_boss_hit
+        bullets_eating_onetime = len(pygame.sprite.groupcollide(self.boss_group,self.hero.bullets,False,True))
+        self.boss.bulletseating += bullets_eating_onetime
+        if self.boss.hp_left >= 0 and bullets_eating_onetime >= 0:
+            self.boss.hp_left -= self.boss.dHP * bullets_eating_onetime
+            #print(self.boss.hp_left)
+            pygame.draw.rect(self.boss.image, (0, 0, 0), (0, 0, self.boss.rect.width, 10), 10)
+            pygame.draw.rect(self.boss.image, (0, 255, 0), (0, 0, self.boss.hp_left, 10), 10)
+        if self.boss.bulletseating > BULLET_HIT_BOSS_TIMES:
+
+            if self.boss.bossexplode_index ==0:
+                self.boss.bossexplode_index =1
+        if self.boss.bossexplode_index == 7:
+            if len(self.boss_group) == 1 and self.boss.exist:
+                score +=1000
+
+            self.boss.kill()
+            self.boss.exist=False
+            self.__game_win()
+
+    def __update_sprites(self):
+        self.screen.blit(game_font.render(u'score: %d' % score, True, [255, 0, 0]), [20, 20])
+        pygame.display.update()
+
+        self.back_group.update()
+        self.back_group.draw(self.screen)
+
+        self.enemy_group.update()
+        self.enemy_group.draw(self.screen)
+
+        self.hero_group.update()
+        self.hero_group.draw(self.screen)
+
+        self.hero.bullets.update()
+        self.hero.bullets.draw(self.screen)
+
+        self.boss_group.update()
+        self.boss_group.draw(self.screen)
+
+        self.boss.weapons.update()
+        self.boss.weapons.draw(self.screen)
+
+    def __game_over(self):
+        keys_pressed = pygame.key.get_pressed()
+        pygame.key.get_mods()
+        self.screen.blit(game_font_over.render(u'Game Over.', True, [255, 0, 0]), [500, 300])
+        self.screen.blit(game_font_over.render(u'Congratulations! Earth is gone ^-^', True, [255, 0, 0]), [300, 400])
+        self.screen.blit(game_font_start.render(u'Press Esc to exit', True, [0, 255, 0]), [980, 750])
+        if keys_pressed[pygame.K_ESCAPE]:
+            pygame.quit()
+            exit()
+
+    def __game_win(self):
+        keys_pressed = pygame.key.get_pressed()
+        pygame.key.get_mods()
+        self.screen.blit(game_font_over.render(u'Earth is safe now!!!', True, [255, 0, 0]), [500, 400])
+        self.screen.blit(game_font_start.render(u'Press Esc to exit', True, [0, 255, 0]), [980, 750])
+        if keys_pressed[pygame.K_ESCAPE]:
+            pygame.quit()
+            exit()
+
+if __name__ == '__main__':
+
+    game = AvengersWar()
+    game.start_game()
---
+++ 551_project/plane_sprites.py
@@ -0,0 +1,229 @@
+import random
+import pygame
+# constant
+# Screen size
+SCREEN_RECT = pygame.Rect(0,0,1200,800)
+# Frame rate
+FRAME_PER_SEC = 60
+# Enemy timer
+CREATE_ENEMY_EVENT = pygame.USEREVENT
+# Launch bullets
+HERO_FIRE_EVENT = pygame.USEREVENT + 1
+# Boss timer
+CREATE_BOSS_EVENT =pygame.USEREVENT+2
+# Bubble timer
+CREATE_BUBBLE_EVENT = pygame.USEREVENT +3
+# Bullet_hit_boss
+BULLET_HIT_BOSS_TIMES = 20
+# Boss blood
+HPFULL = 500
+# Enemy_hit_dict
+enemy_hit_dict = dict()
+
+# Font
+pygame.init()
+game_font = pygame.font.SysFont('arial',16,True)
+
+class GameSprite(pygame.sprite.Sprite):
+    """Sprites"""
+
+    def __init__(self, image_name, speed = 2,speedy = 0):
+
+        # call Father class init
+        super().__init__()
+
+        # Define the attribute
+        self.image = pygame.image.load(image_name)
+        self.rect = self.image.get_rect()
+        self.speed = speed
+        self.speedy = speedy
+
+    def update(self):
+
+        # Move in vertical direction
+        self.rect.y +=self.speed
+
+class Background(GameSprite):
+    """Background Sprite"""
+    def __init__(self,is_alt= False):
+        # 1.Call Father class init
+        super().__init__('./picture/background4.png')
+        # Judge whether it is the same picture. If so, set the initial location
+        if is_alt:
+            self.rect.x = -self.rect.width
+
+    def update(self):
+
+        # 1.Call Father class init
+        # super().update()
+        self.rect.x +=self.speed
+        # 2.Judge whether the picture is out of the screen. If so, put the picture above the screen.
+        if self.rect.x>=SCREEN_RECT.width:
+            self.rect.x = -self.rect.width
+
+class Enemy(GameSprite):
+    """Enemy sprite"""
+
+    def __init__(self):
+
+        #1.Call Father class init,set image and speed
+        super().__init__('./picture/ufo1.png')
+        #2.set random speed
+        self.speed = random.randint(1,6)
+        #3.set random location
+        self.rect.bottom = 0
+        self.explode_index = 0
+
+        max_x = SCREEN_RECT.width - self.rect.width
+        self.rect.x = random.randint(0,max_x)
+
+    def update(self):
+
+        #1.
+        super().update()
+        #2.Judge whether the picture is out of the screen.
+        if self.rect.y>=SCREEN_RECT.height:
+            #print('fly out of the screen ,delete it.')
+            self.kill()
+
+
+        #enemy explosion
+        if self.explode_index!= 0 and self.explode_index <  4:
+            new_rect = self.rect
+            super().__init__('./picture/bomb1%d.png'% self.explode_index)
+            self.explode_index += 1
+            self.rect = new_rect
+
+
+    def __del__(self):
+
+        pass
+
+class Hero(GameSprite):
+    """Hero Sprite"""
+    def __init__(self,path):
+
+        # 1.Call Father class init,set image and speed
+        super().__init__(path, 0)
+        # 2. Set hero initial location
+        self.rect.centerx = SCREEN_RECT.centerx
+        self.rect.bottom = SCREEN_RECT.bottom-120
+        # 3.Create bullets sprite
+        self.bullets = pygame.sprite.Group()
+        self.clock = pygame.time.Clock()
+        self.mainexplode_index =0
+
+    def update(self):
+        # move in horizontal direction
+        self.rect.x += self.speed
+        self.rect.y +=self.speedy
+        # Judge whether the picture is out of the screen. If so, put the picture above the screen.
+        if self.rect.x < 0 :
+            self.rect.x = 0
+        elif self.rect.right > SCREEN_RECT.right:
+            self.rect.right = SCREEN_RECT.right
+        if self.rect.y < 0 :
+            self.rect.y = 0
+        elif self.rect.bottom > SCREEN_RECT.bottom:
+            self.rect.bottom = SCREEN_RECT.bottom
+
+        # Explosion
+        if self.mainexplode_index !=0 and self.mainexplode_index <4:
+            new_rect = self.rect
+            super().__init__('./picture/bomb1%d.png'% self.mainexplode_index)
+            self.rect.centerx = new_rect.centerx
+            self.rect.bottom = new_rect.bottom
+            self.mainexplode_index += 1
+            self.rect = new_rect
+
+    def fire(self):
+        for i in (0,1,2):
+            # 1.Create bullet sprite
+            bullet= Bullet()
+            if self.mainexplode_index ==4:
+                bullet.kill()
+            # 2.Set the initial location
+            bullet.rect.centerx = self.rect.centerx+30
+            bullet.rect.centery = self.rect.centery-140+i*40
+            # 3. Add bullets sprite into sprite group
+            self.bullets.add(bullet)
+
+class Bullet(GameSprite):
+    """Bullet Sprite"""
+    def __init__(self):
+        # Call Father class init,set image and speed
+        super().__init__('./images/bullet1.png',-2)
+
+    def update(self):
+        # Call Father class update
+        super().update()
+        # Judge whether the picture is out of the screen.
+        if self.rect.bottom<0:
+            self.kill()
+
+
+    def __del__(self):
+        pass
+
+class Boss(GameSprite):
+    """boss sprite
+    """
+    def __init__(self):
+        # 1.Call Father class init,set image
+        super().__init__('./picture/thanos1.png')
+        # 2.Set initial location
+        self.rect.centerx = SCREEN_RECT.centerx
+        self.rect.top = SCREEN_RECT.top+20
+        # 3. Create weapon sprite
+        self.weapons = pygame.sprite.Group()
+        self.exist = False
+        self.bossexplode_index = 0
+        self.bulletseating = 0
+        self.hp_left = self.rect.width
+        HPFULL = self.rect.width
+        self.dHP = HPFULL / BULLET_HIT_BOSS_TIMES
+        self.oldpercent = 0
+
+    def update(self):
+        self.rect.x +=self.speed
+        if self.rect.x < 0 :
+            self.rect.x = 0
+            self.speed=-self.speed
+        elif self.rect.right > SCREEN_RECT.right:
+            self.rect.right = SCREEN_RECT.right
+            self.speed=-self.speed
+        if self.exist == False:
+            self.weapons.empty()
+
+        pygame.draw.rect(self.image, (0, 255, 0), (0, 0, self.rect.width, 10), 1)
+
+
+        # boss explosion
+        if self.bossexplode_index !=0 and self.bossexplode_index < 7:
+            new_rect = self.rect
+            super().__init__('./picture/bossbomb%d.png' % self.bossexplode_index)
+            self.rect.centerx = new_rect.centerx
+            self.rect.bottom = new_rect.bottom
+            self.bossexplode_index += 1
+            self.rect = new_rect
+
+    def bubble(self):
+        for i in (0,1):
+            weapon = Weapon()
+            weapon.rect.bottom = self.rect.bottom+50
+            weapon.rect.centerx = self.rect.centerx+20-i*20
+            self.weapons.add(weapon)
+
+class Weapon(GameSprite):
+    """weapon sprite"""
+    def __init__(self):
+        super().__init__('./images/enemy3_down6.png',10)
+    def update(self):
+        super().update()
+        if self.rect.y>SCREEN_RECT.height:
+            self.kill()
+
+    def __del__(self):
+        #print('weapon delete')
+        pass
+
